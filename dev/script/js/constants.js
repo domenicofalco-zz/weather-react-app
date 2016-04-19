06de21475663eb639e73db2f6e4e34b4560b4101
Domenico Falco <domenico.falco@diva-e.com>
2016-04-19 11:14:14 +0200

    commit promise

--- dev/script/js/constants.js
+++ dev/script/js/constants.js
@@ -1,8 +1,9 @@
 /*
-  Example url: api.openweathermap.org/data/2.5/weather?q=Napoli,it&appid=89d31aa95011d4969ac3f7cc20bfd2e5
+  Example url: http://api.openweathermap.org/data/2.5/weather?q=Napoli,it&appid=89d31aa95011d4969ac3f7cc20bfd2e5
 */
 
-const BASE_URI = 'api.openweathermap.org/data/2.5/weather?q=';
+const BASE_URI = 'http://api.openweathermap.org/';
+const QUERY = 'data/2.5/weather?q=';
 const TOKEN = '89d31aa95011d4969ac3f7cc20bfd2e5';
 
-export { BASE_URI, TOKEN };
+export { BASE_URI, QUERY, TOKEN };
